5c2db943df8f7354f7d6b7c086735d2ff2387083
Serg <sergvpan@yandex.ru>
2022-05-19 15:05:21 +0300

    выполнить ТЗ спринта 9

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,12 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class User {
 
     private final Long id;
@@ -16,4 +17,12 @@ public class User {
     private String name;
     private final LocalDate birthday;
     private Set<Long> friends;
+
+    public User(Long id, String email, String login, String name, LocalDate birthday) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
